5cd16913f2f9ba0b1c484036cf80b2842453c4e1
Connor Demille <subtlepseudonym@gmail.com>
2019-07-22 15:59:02 -0400

    Add additional stats and dict functions to get them

--- flow.py
+++ flow.py
@@ -18,7 +18,9 @@ current_milli_time = lambda: int(time.time() * MS_PER_SECOND)
 class FlowMeter:
     totalEvents = 0
     lastEvent = 0 # in milliseconds
-    totalFreq = 0.0 # in 1/ms
+    totalPourEvents = 0
+    totalPourTime = 0.0 # in milliseconds
+    totalFreq = 0.0 # in 1 / seconds
     totalFlow = 0.0 # in liters per second
     totalPour = 0.0 # in liters
     remainingVolume = 0 # will be initialized by json file
@@ -30,12 +32,27 @@ class FlowMeter:
         with open(filepath) as f:
             data = json.load(f)
             self.totalEvents = data['totalEvents']
+            self.totalPourTime = data['totalPourTime']
             self.totalFreq = data['totalFreq']
             self.totalFlow = data['totalFlow']
             self.totalPour = data['totalPour']
             self.remainingVolume = data['remainingVolume']
 
         self.lastEvent = current_milli_time()
+
+    def toJSON(self):
+        return { 'totalEvents': self.totalEvents,
+                 'totalPourEvents': self.totalPourEvents,
+                 'totalPourTime': self.totalPourTime,
+                 'totalFreq': self.totalFreq,
+                 'totalFlow': self.totalFlow,
+                 'totalPour': self.totalPour,
+                 'remainingVolume': self.remainingVolume }
+
+    def getStats(self):
+        return { 'avgFreq': self.totalFreq / self.totalEvents if self.totalEvents else 0,
+                 'avgFlow': self.totalFlow / self.totalEvents if self.totalEvents else 0,
+                 'avgPour': self.totalPourEvents / self.totalEvents if self.totalEvents else 0 }
 
     def update(self, now):
         eventDelta = max((now - self.lastEvent), 1)
@@ -49,8 +66,14 @@ class FlowMeter:
             flow = hz / (SECONDS_PER_MINUTE * 21) # frequency / (seconds_per_minute * flow_meter_constant)
             self.totalFlow += flow
 
-            pour = flow * (eventDelta / MS_PER_SECOND) # in liters
+            pourTime = eventDelta / MS_PER_SECOND
+            self.totalPourTime += pourTime
+
+            pour = flow * pourTime # in liters
             self.totalPour += pour # pour is (1/1260)L per event
+            self.remainingVolume -= pour
+        else:
+            self.totalPourEvents += 1
 
         self.lastEvent = now
 
@@ -66,5 +89,6 @@ GPIO.setup(FLOW_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.add_event_detect(FLOW_PIN, GPIO.RISING, callback=flowMeter.flowEvent, bouncetime=20)
 
 while True:
-    print(flowMeter.totalPour)
+    print("state:", json.dumps(flowMeter.toJSON()))
+    print("stats:", json.dumps(flowMeter.getStats()))
     time.sleep(1)
